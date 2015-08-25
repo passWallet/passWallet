b692772e7b49aa15ca1f0daf71651ffa0ae140e1
KemCake <santos.remi@icloud.com>
2015-08-25 21:10:26 +0200

    Add basic UI 🎅

--- app/js/components/routes/Home.jsx
+++ app/js/components/routes/Home.jsx
@@ -5,7 +5,7 @@ var Home = React.createClass({
   
   getInitialState: function() {
     return {
-    
+      loading: false
     }
   },
   
@@ -13,14 +13,28 @@ var Home = React.createClass({
 
   },
   
+  addToPassbook: function() {
+    this.setState({loading: true});
+    console.log('adding...');
+  },
+  
   //🚗🐰🐇😎👏
   render: function() {    
     return (
       <div className="home">
-        <h1 className=""></h1>
+        <h1 className="">PassWallet</h1>
         <p className="subtitle">
-         
+          {"Keep your bitcoin wallet in Passbook"}
         </p>
+        <br/>
+        <br/>
+        <label className="address">Wallet Addresse</label>
+        <input type="text" placeholder="Wallet public address"/>
+        <br/><br/>
+        <label className="description">Description</label>
+        <input type="text" placeholder="Description (optional)"/>
+        <br/><br/>
+        <button onClick={this.addToPassbook}>Add to Passbook</button>
       </div>
     );
   },
